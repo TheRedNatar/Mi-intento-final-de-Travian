340db11604c7dbfdcd4619dc0e15a3102dbc26ff
SirWerto <sirwerto@gmail.com>
2023-07-23 22:46:39 +0200

    add urls to the table

--- apps/t_types/lib/t_types.ex
+++ apps/t_types/lib/t_types.ex
@@ -166,4 +166,18 @@ defmodule TTypes do
 
   @spec distance401(x1 :: float(), y1 :: float(), x2 :: float(), y2 :: float()) :: float()
   def distance401(x1, y1, x2, y2), do: distance(401, 401, x1, y1, x2, y2)
+
+
+
+  @spec player_url(server_id :: server_id(), player_id :: player_id()) :: String.t()
+  def player_url(server_id, player_id) do
+    [_, _, player_identifier] = String.split(player_id, "--", parts: 3)
+    "#{server_id}/profile/#{player_identifier}"
+  end
+
+  @spec alliance_url(server_id :: server_id(), alliance_id :: alliance_id()) :: String.t()
+  def alliance_url(server_id, alliance_id) do
+    [_, _, alliance_identifier] = String.split(alliance_id, "--", parts: 3)
+    "#{server_id}/alliance/#{alliance_identifier}"
+  end
 end
