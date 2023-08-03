acbee8bd9531a3fde78491828b3b298c4437f888
Jorge Vergés Montano <sirwerto@gmail.com>
2023-08-03 17:22:28 +0200

    add_scripts_module

--- apps/collector/lib/collector.ex
+++ apps/collector/lib/collector.ex
@@ -15,26 +15,4 @@ defmodule Collector do
   """
   @spec subscribe() :: reference()
   def subscribe(), do: Collector.GenCollector.subscribe()
-
-  def set_up_mnesia(nodes, master_node) do
-    :rpc.multicall(nodes, :application, :stop, [:mnesia])
-
-    with(
-      :ok <- :mnesia.delete_schema(nodes),
-      :ok <- :mnesia.create_schema(nodes),
-      :rpc.multicall(nodes, :application, :start, [:mnesia]),
-      {:atomic, _res} <- Collector.Feed.create_table(nodes, Collector.SServer),
-      {:atomic, _res} <- Collector.Feed.create_table(nodes, Collector.SMedusaPred),
-      :ok <- :mnesia.set_master_nodes([master_node])
-    ) do
-      :ok
-    else
-      reason -> {:error, reason}
-    end
-  end
-
-  def clear_tables() do
-    :mnesia.clear_table(:s_server)
-    :mnesia.clear_table(:s_medusa_pred)
-  end
 end
