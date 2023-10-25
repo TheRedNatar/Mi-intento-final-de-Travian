4cbb0e066beef0dc080e9dd5402c7e6daa0b95ea
SirWerto <sirwerto@gmail.com>
2023-10-25 12:34:30 +0200

    docs(*): just to test the releas action

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MyTravian.MixProject do
     [
       name: "MyTravian project",
       apps_path: "apps",
-      version: "0.2.0",
+      version: "0.2.1",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
